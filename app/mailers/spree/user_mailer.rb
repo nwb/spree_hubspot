0f766526dcbc39cf686c8998325272f356b67fc1
Dustin Eichler <dustin@smalhaus.com>
2019-03-01 15:11:51 -0500

    Update Spree UserMailer
    Use conditional assignment

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -5,14 +5,13 @@ module Spree
     def reset_password_instructions(user, token, *_args)
       @edit_password_reset_url = spree.edit_spree_user_password_url(reset_password_token: token, host: Spree::Store.current.url)
 
-      current_store = Spree::Store.current
-
-      if current_store.url.include? ".uk"
-        email_id = SpreeHubspot::Config.uk_password_reset_email_id
-      elsif current_store.url.include? ".au"
-        email_id = SpreeHubspot::Config.aus_password_reset_email_id
-      elsif current_store.url.include? ".eu"
-        email_id = SpreeHubspot::Config.eu_password_reset_email_id
+      email_id = if from_store.url.include? ".uk"
+          SpreeHubspot::Config.uk_password_reset_email_id
+        elsif from_store.url.include? ".au"
+          SpreeHubspot::Config.aus_password_reset_email_id
+        elsif from_store.url.include? ".eu"
+          SpreeHubspot::Config.eu_password_reset_email_id
+        end
       end
 
       mail(email_id: email_id, message: { to: user.email }) if email_id
@@ -22,6 +21,10 @@ module Spree
 
     def from_address
       "no-reply@example.com"
+    end
+
+    def from_store
+      @store ||= Spree::Store.current
     end
   end
 end
