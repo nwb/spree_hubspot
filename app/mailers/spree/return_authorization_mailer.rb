d1d0d13073942cf7a4026b398b57d907b76c5949
Dustin Eichler <dustin@smalhaus.com>
2019-03-05 13:04:18 -0500

    Add custom_properties, contact_properties

--- app/mailers/spree/return_authorization_mailer.rb
+++ app/mailers/spree/return_authorization_mailer.rb
@@ -13,7 +13,21 @@ module Spree
         email_id = SpreeHubspot::Config.eu_rma_email_id
       end
 
-      mail(email_id: email_id, message: { to: user.email }) if email_id
+      contact_properties = []
+
+      custom_properties = [
+        { name: "id", value: return_authorization.id },
+        { name: "number", value: return_authorization.number },
+        { name: "state", value: return_authorization.state },
+        { name: "order_id", value: return_authorization.order_id },
+        { name: "memo", value: return_authorization.memo },
+        { name: "created_at", value: return_authorization.created_at },
+        { name: "updated_at", value: return_authorization.updated_at },
+        { name: "stock_location_id", value: return_authorization.stock_location_id },
+        { name: "return_authorization_reason_id", value: return_authorization.return_authorization_reason_id }
+      ]
+
+      mail(email_id: email_id, message: { to: user.email }, contact_properties: contact_properties, custom_properties: custom_properties) if email_id
     end
 
     private
