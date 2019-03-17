c2653ba5c72c202c6fcd5503afac4b5c3a27b3bd
Albert Liu <albertliu@naturalwellbeing.com>
2019-03-17 14:53:09 -0700

    newsletter route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
+  #   post "/subscribenewsletter", :to => 'home#subscribenewsletter'
+  post "/subscribenewsletters", :to => 'home#subscribenewsletters'
+  post "/subscribecampaign", :to => 'home#subscribecampaign'
+  post "/subscribecampaign_with_ops", :to => 'home#subscribecampaign_with_ops'
+
+  post "/subscribekeyword", :to => 'home#subscribekeyword'
 end
