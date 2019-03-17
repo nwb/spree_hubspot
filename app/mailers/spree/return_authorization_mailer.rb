fe1ccf893d4c2cd00411ebff7e671e651096e4ce
beckyl <becky@naturalwellbeing.com>
2019-03-17 15:16:12 -0700

    add rma custom_properties

--- app/mailers/spree/return_authorization_mailer.rb
+++ app/mailers/spree/return_authorization_mailer.rb
@@ -4,7 +4,7 @@ module Spree
   class ReturnAuthorizationMailer < Hubspot::TransactionEmail::Mailer
     def email(to_email, return_authorization, *_args)
 
-      email_id = case Spree::Store.current.code
+      email_id = case return_authorization.order.store.code
         when "nwb", "pwb", "he"
           SpreeHubspot::Config.com_rma_email_id
         when "pwbca"
@@ -17,6 +17,17 @@ module Spree
           SpreeHubspot::Config.eu_rma_email_id
         end
 
+      if return_authorization.return_authorization_reason_id.nil?
+        return_reason = return_authorization.memo
+      else
+        return_reason =Spree::ReturnAuthorizationReason.find(return_authorization.return_authorization_reason_id).name
+      end
+
+      returnunits=[]
+      return_authorization.return_items.each do |return_item|
+          returnunits << {:sku => return_item.inventory_unit.variant.sku, :return_quantity => return_item.inventory_unit.quantity, :return_state => return_item.return_state }
+      end
+
       contact_properties = []
 
       custom_properties = [
@@ -28,7 +39,9 @@ module Spree
         { name: "created_at", value: return_authorization.created_at },
         { name: "updated_at", value: return_authorization.updated_at },
         { name: "stock_location_id", value: return_authorization.stock_location_id },
-        { name: "return_authorization_reason_id", value: return_authorization.return_authorization_reason_id }
+        { name: "return_authorization_reason_id", value: return_authorization.return_authorization_reason_id },
+        { name: "return_reason", value: return_reason},
+        { name: "return_items", value: returnunits}
       ]
 
       mail(email_id: email_id, message: { to: to_email }, contact_properties: contact_properties, custom_properties: custom_properties) if email_id
